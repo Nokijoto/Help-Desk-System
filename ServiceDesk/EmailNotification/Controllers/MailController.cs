f9dfeca88426bff2d3b8d96ac28300619f353f7d
PawelZralka1 <pawel.z.675@gmail.com>
2024-07-12 20:25:40 +0200

    Maile pierwsza wersja

---
+++ ServiceDesk/EmailNotification/Controllers/MailController.cs
@@ -0,0 +1,24 @@
+﻿using EmailNotification.Models;
+using EmailNotification.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailNotification.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MailController : ControllerBase
+    {
+        IMailService Mail_Service = null;
+        //injecting the IMailService into the constructor
+        public MailController(IMailService _MailService)
+        {
+            Mail_Service = _MailService;
+        }
+        [HttpPost]
+        public bool SendMail(MailData Mail_Data)
+        {
+            return Mail_Service.SendMail(Mail_Data);
+        }
+    }
+}
---
+++ ServiceDesk/EmailNotification/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using EmailNotification.Models;
+
+namespace EmailNotification.Services
+{
+    public interface IMailService
+    {
+        bool SendMail(MailData Mail_Data);
+    }
+}
